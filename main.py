91ae6d06f65c40fe5c31506d9631675c691d322a
somu <ravurisomasekhar@gmail.com>
2025-08-02 15:32:33 +0530

    fixed error in search method.

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ class WebCrawler:
     def search(self, keyword):
         results = []
         for url, text in self.index.items():
-            if keyword.lower() not in text.lower():
+            if keyword.lower() in text.lower():
+                # not in is present instead of only in
                 results.append(url)
         return results
 
